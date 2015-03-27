96edcad5e242a762608dd4961709fd9e851bc54c
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-03-27 22:19:22 +0100

    Tests: smoke test for checking if library is exposed as global property

--- tests/unit/smoke.js
+++ tests/unit/smoke.js
@@ -5,13 +5,15 @@ define([
 	"intern/chai!assert",
 	"require",
 	"src/qunit-desktop-notifications"
-], function ( registerSuite, assert, require, qunit, qunitDesktopNotifications ) {
+], function ( registerSuite, assert, require, qunit ) {
 	registerSuite({
 		name: "Smoke test",
 		"QUnitDesktopNotifications is created.": function () {
+			/** Smoke test checking if QUnitDesktopNotifications is executed without syntax errors */
 			assert.equal( typeof self.QUnitDesktopNotifications, "object", "OK" );
 		},
 		"QUnitDesktopNotifications is exposed as window property.": function () {
+			/** Smoke test checking kind of the same as the previous test, but with browser context. */
 			if ( typeof window !== "undefined" ) {
 				assert.equal( typeof window.QUnitDesktopNotifications, "object", "OK" );
 			} else {
