fef595df122fa698fedf5b1370c753459109c8bb
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-05-02 16:57:53 +0200

    Reverting to Firefox 35

--- tests/intern-ci.js
+++ tests/intern-ci.js
@@ -14,7 +14,7 @@ define({
 		port: 4444
 	},
 	environments: [
-		{ browserName: "Firefox", platform: "Windows 7", version: "37" },
+		{ browserName: "Firefox", platform: "Windows 7", version: "35" },
 		{ browserName: "Chrome" }
 	],
 	maxConcurrency: 3,
